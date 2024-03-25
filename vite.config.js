43ea1489ca86d078f5c1ea3eb770a84b5e62db16
Your Name <you@example.com>
2024-03-25 09:00:23 +0200

    New project

---
+++ vite.config.js
@@ -0,0 +1,37 @@
+import { defineConfig } from 'vite'
+import handlebars from 'vite-plugin-handlebars'
+import { resolve } from 'path'
+import FullReload from 'vite-plugin-full-reload'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
+import injectHTML from 'vite-plugin-html-inject'
+
+export default defineConfig({
+	build: {
+		target: 'es2022',
+		outDir: 'build',
+		// rollupOptions: {
+		// 	input: {
+		// 		...pagesInput
+		// 	}
+		// }
+	},
+
+	server: {
+		port: 3025,
+		host: '0.0.0.0',
+		hmr: true
+	},
+	plugins: [
+		injectHTML(),
+		handlebars({
+			reloadOnPartialChange: true,
+			partialDirectory: resolve(__dirname, 'src/html-partials')
+		}),
+
+		FullReload('src/html-partials/**/*', { delay: 0 }),
+
+		viteStaticCopy({
+			targets: []
+		})
+	]
+})
