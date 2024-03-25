9fd45fd36cf89be95eca2901f253e1d00481c2cf
Your Name <you@example.com>
2024-03-25 14:24:28 +0200

    adding animation and adaptive block

--- src/js/header.js
+++ src/js/header.js
@@ -1,8 +1,10 @@
 export function openBurgerMenu() {
 	const burger = document.querySelector('.header__burger')
 	const headerMenu = document.querySelector('.header__menu')
+	const body = document.querySelector('body')
 	burger.addEventListener('click', function () {
 		burger.classList.toggle('active')
 		headerMenu.classList.toggle('open')
+		body.classList.toggle('lock')
 	})
 }
